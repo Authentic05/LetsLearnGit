47c51e3ae6bed6580d91bbc3b1a5395a513c658e
Suhas <shiv2506@gmail.com>
2023-04-03 00:04:25 +0530

    Added the Multiply class

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int e=50;
+        int f=2;
+        System.out.println("Multiplication of the two number is="+(e*f));
+    }
+}
